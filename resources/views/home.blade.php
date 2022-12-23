bd1287053a632474d6525f5b1eac7c9860588d97
杉本悠哉 <y.sugimot358@gmail.com>
2022-12-23 10:26:37 +0900

    ホーム画面作成の対応

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,12 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
-        </div>
+    <div class="rule" style="background-color:rgb(222, 225, 225); height:55px; border-radius:30px">
+        <p class="text-center fw-bold pt-3">分からない問題は解答しないこと！！</p>
+    </div>
+    <div class="button text-center mt-5">
+        <button type="button" class="btn btn-success" style="width: 45%; height:150px; margin-right:10px; border-radius:20px">確認テスト</button>
+        <button type="button" class="btn btn-success" style="width: 45%; height:150px; border-radius:20px">練習問題</button>
     </div>
 </div>
 @endsection
