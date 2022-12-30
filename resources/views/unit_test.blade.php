b5af6a4844532a04b6b888db1ae3d7c710687fa2
杉本悠哉 <y.sugimot358@gmail.com>
2022-12-30 11:20:17 +0900

    各参考書ごとに紐づく単元のテストだけを表示する実装の対応

--- resources/views/unit_test.blade.php
+++ resources/views/unit_test.blade.php
@@ -8,7 +8,7 @@
 </header>
 <div class="button text-center">
     @foreach($UnitTest as $UnitTest)
-    <a href="#"><button type="button" class="btn btn-success" style="width: 90%; height:150px; margin-top:20px; border-radius:20px">{{$UnitTest->name}}</button></a>
+    <a href=""><button type="button" class="btn btn-success" style="width: 90%; height:150px; margin-top:20px; border-radius:20px">{{$UnitTest->name}}</button></a>
     @endforeach
 </div>
 @endsection
