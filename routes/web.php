8c18619cbf1175d57cb74bca35ad06090aa5cf82
杉本悠哉 <y.sugimot358@gmail.com>
2023-01-01 17:36:55 +0900

    モデル名変更に伴うURL変更の対応

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,8 @@ use App\Http\Controllers\SubjectTestController;
 use App\Http\Controllers\SubjectPracticeController;
 use App\Http\Controllers\ReferenceBookTestController;
 use App\Http\Controllers\ReferenceBookPracticeController;
-use App\Http\Controllers\UnitController;
+use App\Http\Controllers\UnitTestController;
+use App\Http\Controllers\UnitPracticeController;
 use App\Http\Controllers\IssueController;
 
 /*
@@ -35,5 +36,6 @@ Route::get('/subject_test', [SubjectTestController::class, 'subject_test']);
 Route::get('/subject_practice', [SubjectPracticeController::class, 'subject_practice']);
 Route::get('/reference_book_test', [ReferenceBookTestController::class, 'reference_test']);
 Route::get('/reference_book_practice', [ReferenceBookPracticeController::class, 'reference_practice']);
-Route::get('/unit/{reference_book_id}', [UnitController::class, 'unit']);
+Route::get('/unit_test/{reference_book_id}', [UnitTestController::class, 'unit_test']);
+Route::get('/unit_practice/{reference_book_id}', [UnitPracticeController::class, 'unit_practice']);
 Route::get('/issue', [IssueController::class, 'issue']);
