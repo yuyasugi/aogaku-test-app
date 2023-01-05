fa3f12adf4f49d0f313cd18c90679025651bbad1
杉本悠哉 <y.sugimot358@gmail.com>
2023-01-05 10:32:27 +0900

    解答正誤判定機能作成の対応

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Issue;
+use App\Models\IssueResult;
 use App\Models\ReferenceBook;
 use App\Models\UnitTest;
 use Illuminate\Http\Request;
@@ -11,8 +13,18 @@ use Illuminate\Support\Facades\DB;
 class ResultController extends Controller
 {
     public function result(Request $request){
+        $issue = new Issue;
         $posts = $request->all();
-        dd($posts);
+        $issues = $issue->get_all_issue();
+
+        foreach( $issues as $index => $issue){
+            $correct =  $issue->anser === $posts[$index+1];
+            if($correct){
+            IssueResult::create(['user_id' => 1, 'unit_id' => 1, 'issue_id' => 1, 'correct'=> true]);
+            }
+            IssueResult::create(['user_id' => 1, 'unit_id' => 1, 'issue_id' => 1,'correct'=> false]);
+            };
+            dd($issues);
         return view('result');
      }
 }
