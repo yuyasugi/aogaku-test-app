11215ae4ef09f5db1a0c6b40305cf1654adf4f55
杉本悠哉 <y.sugimot358@gmail.com>
2023-01-06 17:10:29 +0900

    各単元ごとの練習問題表示の対応

--- resources/views/issue.blade.php
+++ resources/views/issue.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <form class="issue-box pt-3" action="{{ route('result') }}" method="POST">
     @csrf
-    @foreach($Issue as $Issue)
+    @foreach($UnitIssue as $Issue)
     <p>{{$Issue->problem}}</p>
     <input class="form-control mb-3" name="anser" type="text" placeholder="解答を入力" aria-label="default input example">
     @endforeach
