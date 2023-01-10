2e94fd7c9a5fe76082d6cffc78b553980ec35af6
杉本悠哉 <y.sugimot358@gmail.com>
2023-01-10 10:59:08 +0900

    Revert "解答の正誤判定機能作成の対応"
    This reverts commit 5e2b406a3a40d5f7f99e76698495b56586f1995d.

--- app/Http/Controllers/ResultController.php
+++ app/Http/Controllers/ResultController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Issue;
 use App\Models\IssueResult;
 use App\Models\ReferenceBook;
-use App\Models\Unit;
+use App\Models\UnitTest;
 use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\DB;
@@ -14,30 +14,17 @@ class ResultController extends Controller
 {
     public function result(Request $request){
         $issue = new Issue;
-        $posts = $request->all();//⇦10番の問題の解答しか入ってこない
-        $issues = Issue::select()
-                    ->join('unit_issues', 'unit_issues.issue_id', '=', 'issues.id')
-                    ->where('unit_id', 1)
-                    ->get();
-        //ここの$issuesで全ての問題が取れてしまっている。unit_idで指定したい
+        $posts = $request->all();
+        $issues = $issue->get_all_issue();
 
         foreach( $issues as $index => $issue){
             $correct =  $issue->anser === $posts[$index+1];
-            if($correct == true){
-            IssueResult::create(['user_id' => 1, 'unit_id' => 1, 'issue_id' => $issue->id, 'correct'=> true]);
-            } else{
-                IssueResult::create(['user_id' => 1, 'unit_id' => 1, 'issue_id' => $issue->id,'correct'=> false]);
+            if($correct){
+            IssueResult::create(['user_id' => 1, 'unit_id' => 1, 'issue_id' => 1, 'correct'=> true]);
             }
-        };
-
-        $issue_result = Issue::select()
-            ->join('issue_results','issue_results.issue_id','=','issues.id')
-            ->where('correct',0)
-            ->get();
-
-        $issue_count = IssueResult::count('correct');
-        $score = IssueResult::where('correct', '=', 1)->count();
-
-        return view('result',compact(['issue_result', 'issue_count', 'score']));
+            IssueResult::create(['user_id' => 1, 'unit_id' => 1, 'issue_id' => 1,'correct'=> false]);
+            };
+            dd($issues);
+        return view('result');
      }
 }
