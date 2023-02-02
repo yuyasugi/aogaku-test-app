7a701b599347d45a8db53c1afa9d93de62ba2afd
杉本悠哉 <y.sugimot358@gmail.com>
2023-02-02 11:52:10 +0900

    タイトル変更の対応

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>青学コーチング</title>
+    <title>アイウエオ</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
