fba4628d71687ff28c377771c2ece4980865752b
杉本悠哉 <y.sugimot358@gmail.com>
2023-03-24 11:33:08 +0900

    no message

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,24 +1,24 @@
 <?php
 
-namespace App\Http\Middleware;
+// namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Http\Request;
+// use Closure;
+// use Illuminate\Http\Request;
 
-class Cors
-{
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-        return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
-        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type');
-    }
-}
+// class Cors
+// {
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  \Illuminate\Http\Request  $request
+//      * @param  \Closure  $next
+//      * @return mixed
+//      */
+//     public function handle($request, Closure $next)
+//     {
+//         return $next($request)
+//         ->header('Access-Control-Allow-Origin', 'http://localhost:3000')
+//         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+//         ->header('Access-Control-Allow-Headers', 'Content-Type');
+//     }
+// }
