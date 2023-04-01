7be55006c3d0c307f14ae4b0a1000ecf165ca12d
杉本悠哉 <y.sugimot358@gmail.com>
2023-04-01 16:34:14 +0900

    no message

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -19,6 +19,6 @@ class Cors
         return $next($request)
         ->header('Access-Control-Allow-Origin', 'https://aogaku-test-app.sakura.ne.jp')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
-        ->header('Access-Control-Allow-Headers', 'Content-Type');
+        ->header('Access-Control-Allow-Headers', 'Content-Type, X-Auth-Token, Origin, Authorization');
     }
 }
